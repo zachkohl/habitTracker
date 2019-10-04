75453f64b85b79cfeaa3f61c3433ba9b571a46cf
zachkohl <zach.kohl@gmail.com>
2019-10-04 10:20:17 -0700

    got month and started years

--- src/components/Review.js
+++ src/components/Review.js
@@ -27,7 +27,9 @@ class Review extends Component {
 constructor(props){
 super(props);
     this.state = {
+showMonthTable: false,
 dateObject: moment(),
+allMonths : moment.months(),
     }
   
 }
@@ -42,6 +44,109 @@ firstDayOfMonth = () =>{
 
 daysInMonth = (parameter) =>{
     return moment(parameter).daysInMonth();
+}
+
+month = () =>{
+    return this.state.dateObject.format('MMMM');
+}
+
+
+MonthList = props => {
+   let months = [];
+    props.data.map(data => {
+        months.push(
+            <td key={data} onClick={e => {this.setMonth(data)}}>
+                <span>{data}</span>
+            </td>
+        );
+    })
+
+   let rows = [];
+   let cells = [];
+
+months.forEach((row,i) =>{
+    if(i % 3 !==0|| i==0){
+        cells.push(row);
+    }else{
+        rows.push(cells);
+        cells = [];
+        cells.push(row);
+    }
+})
+rows.push(cells);
+
+let monthlist = rows.map((d,i) => {
+    let key = i + '_quarter';
+    return <tr key ={key}>{d}</tr>;
+})
+return monthlist;
+}
+
+setMonth = month =>{
+    let monthNumber = this.state.allMonths.indexOf(month);
+    console.log(monthNumber)
+    let dateObject = Object.assign({},this.state.dateObject);
+    dateObject = moment(dateObject).set('month',monthNumber);
+    this.setState({
+        dateObject: dateObject,
+        showMonthTable: !this.state.showMonthTable
+    })
+};
+
+showMonth = (e, month) =>{
+    this.setState({
+        showMonthTable: !this.state.showMonthTable
+    })
+}
+
+
+year = () =>{
+    return this.state.dateObject.format("Y");
+}
+
+getDates(startDate,stopDate){
+    var dateArray = [];
+    var currentDate = moment(startDate);
+    var stopDate = moment(stopDate);
+    while(currentDate <= stopDate)
+{
+    dateArray.push(moment(currentDate).format("YYYY"))
+    currentDate =moment(currentDate).add(1,"year");
+}
+return dateArray;
+}
+
+yearTable = props =>{
+    let months = [];
+    let nextTen = moment().set("year",props).add("year",12).format("Y");
+
+    let twelveYears = this.getDates(props,nextTen);
+
+    twelveYears.map(data =>{
+        months.push(
+            <td key={data} onClick={e =>{this.setYEar(data)}}>
+                <span>{data}</span>
+            </td>
+        )
+    })
+let rows = [];
+let cells = [];
+
+months.forEach((row,i)=>{
+    if(i % 3!==0 ||i==0){
+        cells.push(row);
+    }else{
+        rows.push(cells);
+        cells=[];
+        cells.push(row);
+    }
+})
+rows.push(cells);
+let yearList = rows.map((d,i)=>{
+    const key = i+300;
+    return <tr key={key}>{d}</tr>
+})
+return <table><thead><tr><th>Select year</th></tr></thead><tbody>{yearList}</tbody></table>
 }
 
 //https://programmingwithmosh.com/react/build-a-react-calendar-component-from-scratch/
@@ -61,7 +166,7 @@ daysInMonth = (parameter) =>{
         }
      
         let daysInMonthLeft = [];
-        for (let d=1; d<=this.daysInMonth(this.dateObject);d++){
+        for (let d=1; d<=this.daysInMonth(this.state.dateObject);d++){
             daysInMonthLeft.push(<td key={d} className="calendar-day">{d}</td>)
            
         }
@@ -92,7 +197,14 @@ daysInMonth = (parameter) =>{
         });
 
         return <div>
-            <table><thead><tr>{weekdayShortName}</tr></thead><tbody>{daysInMonthRows}</tbody></table>
+            <div onClick={e => { this.showMonth()}}>{this.month()}
+            <span>
+            {" "}{<this.yearTable props={this.year()}/>}
+            </span>
+            </div>
+            <div><table><tbody>{this.state.showMonthTable && <this.MonthList data={moment.months()}/>}</tbody></table></div>
+    {!this.state.showMonthTable && ( <table><thead><tr>{weekdayShortName}</tr></thead><tbody>{daysInMonthRows}</tbody></table>)}
+           
         </div>
        
 }
